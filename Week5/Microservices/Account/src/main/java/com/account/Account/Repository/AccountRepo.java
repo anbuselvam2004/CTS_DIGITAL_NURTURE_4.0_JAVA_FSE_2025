0f30197372118b50d32bab0eb970ae745f60af24
anbuselvam2004 <119647662+anbuselvam2004@users.noreply.github.com>
2025-07-19 23:53:06 +0530

    Delete Week5 directory

--- Week5/Microservices/Account/src/main/java/com/account/Account/Repository/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.account.Account.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.account.Account.Model.Account;
-
-public interface AccountRepo extends JpaRepository<Account,String> {
-
-}
